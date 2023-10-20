994fb152110c2bc07ed39c7a6d8b8152f5f6fc35
Akhilesh Varanasi <127005648+a1k2-c3@users.noreply.github.com>
2023-10-20 14:08:32 +0530

    def poli(n):     rev=0     while n>0:         rem=n%10         rev=(rev*10)+rem         n=n//10     return rev n=int(input());x=n a=poli(n) if a==x:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,12 +1,13 @@
-n=int(input())
-s=n
-rev=0
-while n>0:
-    rem=n%10
-    rev=(rev*10)+rem
-    n=n//10
-if rev==s:
-    print('Palindrome')
+def poli(n):
+    rev=0
+    while n>0:
+        rem=n%10
+        rev=(rev*10)+rem
+        n=n//10
+    return rev
+n=int(input());x=n
+a=poli(n)
+if a==x:
+    print(True)
 else:
-    print('Not Palindrome')
-
+    print(False)
